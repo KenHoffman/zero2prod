9ef4b3f68fd4856d12940cd71dbce4b724da7699
Ken Hoffman <khoff98107@yahoo.com>
2025-10-17 01:32:09 -0600

    More changes for chapter 7.

--- migrations/20251012065353_make_status_not_null_in_subscriptions.sql
+++ migrations/20251012065353_make_status_not_null_in_subscriptions.sql
@@ -1,6 +1,11 @@
-CREATE TABLE subscription_tokens(
-    subscription_token TEXT NOT NULL,
-    subscriber_id uuid NOT NULL
-        REFERENCES subscriptions (id),
-    PRIMARY KEY (subscription_token)
-);
+-- We wrap the whole migration in a transaction to make sure
+-- it succeeds or fails atomically.
+-- `sqlx` does not do it automatically for us.
+BEGIN;
+    -- Backfill `status` for historical entries
+UPDATE subscriptions
+SET status = 'confirmed'
+WHERE status IS NULL;
+-- Make `status` mandatory
+ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
+COMMIT;
