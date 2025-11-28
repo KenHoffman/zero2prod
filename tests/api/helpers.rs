aedec9dd450f25f2206a30a98914b0f52b64f4d6
Ken Hoffman <khoff98107@yahoo.com>
2025-11-28 10:03:54 -0700

    Changes for chapter 10.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,5 +1,6 @@
+use argon2::password_hash::SaltString;
+use argon2::{Argon2, PasswordHasher};
 use secrecy::Secret;
-use sha3::Digest;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::sync::LazyLock;
 use uuid::Uuid;
@@ -126,8 +127,13 @@ impl TestUser {
         }
     }
     async fn store(&self, pool: &PgPool) {
-        let password_hash = sha3::Sha3_256::digest(self.password.as_bytes());
-        let password_hash = format!("{:x}", password_hash);
+        let salt = SaltString::generate(&mut rand::thread_rng());
+        // We don't care about the exact Argon2 parameters here
+        // given that it's for testing purposes!
+        let password_hash = Argon2::default()
+            .hash_password(self.password.as_bytes(), &salt)
+            .unwrap()
+            .to_string();
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash)
 VALUES ($1, $2, $3)",
